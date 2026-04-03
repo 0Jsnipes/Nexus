b24cad9f92a838786c1280b523a1d6ba136edb6f
0Jsnipes <jared.c.snipes@gmail.com>
2026-04-02 21:37:33 -0400

    error fixed

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import "./chatList.css";
+import "./ChatList.css";
 import AddUser from "./addUser/addUser"; // Adjusted the path to match the folder structure
 import { useUserStore } from "../../lib/userStore";
 import { doc, getDoc, onSnapshot, updateDoc } from "firebase/firestore"; // Removed unused updateDoc import
